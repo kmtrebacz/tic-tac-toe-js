3a5b08a44c51847a485429374e49f9d0349ca330
Kacper <kacperkapitrebacz@gmail.com>
2023-04-19 20:44:58 +0200

    Cross or circle shows up after click and overwrite the position is impossible

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const gameBoard = document.querySelector('#gameboard')
 const infoDisplay = document.querySelector('#info')
 const cells = document.querySelectorAll('.square')
-const xCells = ["", "", "", "", "", "", "", "", ""]
-const oCells = ["", "", "", "", "", "", "", "", ""]
+const cellsIndex = ["", "", "", "", "", "", "", "", ""]
+const winningSequences = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const xCells = []
+const oCells = []
 let currentTurn = 'x'
 
 console.log(cells)
@@ -12,10 +23,39 @@ cells.forEach(cell => {
           console.log(cell.id)
           console.log(currentTurn)
           switch (currentTurn){
-               case 'x': currentTurn = 'o' 
-                    break
-               case 'o': currentTurn = 'x'
-                    break
+            case 'x': 
+              updateCellsIndex(cell.id, 'x')
+              break
+            case 'o': 
+              updateCellsIndex(cell.id, 'o')
+              break
           }
      })
 })
+
+function updateCellsIndex(id, currentPlayer) {
+  if (cellsIndex[id] == ""){
+    cellsIndex[id] = currentPlayer
+    if (currentPlayer == 'x'){
+      xCells.push(id)
+      currentTurn = 'o'
+    } else {
+      oCells.push(id)
+      currentTurn = 'x'
+    }
+    console.log(cellsIndex)
+    console.log(xCells)
+    console.log(oCells)
+    printCell(id, currentPlayer)
+  } else {
+    alert('Cell is already choosed')
+  }
+}
+
+function printCell(cellId, currentPlayer) {
+  if (currentPlayer == 'x'){
+    document.getElementById(cellId).innerHTML = "<img src='./img/cross.png' style='width: 100%; padding: 20px'>"
+  } else if (currentPlayer == 'o'){
+    document.getElementById(cellId).innerHTML = "<img src='./img/circle.png' style='width: 100%; padding: 20px'>"
+  }
+}
